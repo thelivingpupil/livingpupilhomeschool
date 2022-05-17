211e354f48bb0d122baabffd36232b937b7d3666
Sylvie <sylvie@Sylvie.local>
2022-05-17 16:06:44 +0800

    Facebook Messenger chat plugin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import ErrorPage from 'next/error';
 import { useRouter } from 'next/router';
+import Script from 'next/script';
 
 import Meta from '@/components/Meta/index';
 import { LandingLayout } from '@/layouts/index';
@@ -21,6 +22,30 @@ const Home = ({ page }) => {
     <LandingLayout>
       <Meta title="Living Pupil Homeschool" />
       {render(page.content)}
+      <div id="fb-root"></div>
+      <div id="fb-customer-chat" className="fb-customerchat"></div>
+      <Script strategy="lazyOnload">
+        {`
+          var chatbox = document.getElementById('fb-customer-chat');
+          chatbox.setAttribute("page_id", "174774976550458");
+          chatbox.setAttribute("attribution", "biz_inbox");
+          
+          window.fbAsyncInit = function() {
+            FB.init({
+              xfbml            : true,
+              version          : 'v13.0'
+            });
+          };
+
+          (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+          }(document, 'script', 'facebook-jssdk'));
+        `}
+      </Script>
     </LandingLayout>
   );
 };
