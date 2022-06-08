839848f279344b91320c25cc2c5416dc44d06fee
Sylvie <sylvie@Sylvie.local>
2022-06-08 17:58:52 +0800

    Update homeschool fees

--- src/pages/homeschool-cottage.js
+++ src/pages/homeschool-cottage.js
@@ -1,22 +1,217 @@
+import { useState } from 'react';
+import { ChevronDownIcon } from '@heroicons/react/outline';
+import { Accreditation, Enrollment, GradeLevel } from '@prisma/client';
+
 import Meta from '@/components/Meta/index';
 import { LandingLayout } from '@/layouts/index';
 import sanityClient from '@/lib/server/sanity';
 import Footer from '@/sections/footer';
 import Header from '@/sections/header';
 import Title from '@/sections/sectionTitle';
+import { GRADE_LEVEL, GRADE_LEVEL_GROUPS } from '@/utils/constants';
 
 const HomeschoolCottage = ({ page, fees }) => {
   const { footer, header } = page;
   const [headerSection] = header?.sectionType;
   const [footerSection] = footer?.sectionType;
+  const [enrollmentType, setEnrollmentType] = useState(Enrollment.NEW);
+  const [incomingGradeLevel, setIncomingGradeLevel] = useState(
+    GradeLevel.GRADE_1
+  );
+  const [accreditation, setAccreditation] = useState(Accreditation.FORM_ONE);
+
+  const schoolFee = fees.schoolFees.find((fee) => {
+    let gradeLevel = incomingGradeLevel;
+
+    if (
+      accreditation === Accreditation.FORM_ONE &&
+      (incomingGradeLevel === GradeLevel.GRADE_1 ||
+        incomingGradeLevel === GradeLevel.GRADE_2 ||
+        incomingGradeLevel === GradeLevel.GRADE_3)
+    ) {
+      gradeLevel = GradeLevel.GRADE_3;
+    } else if (
+      accreditation === Accreditation.FORM_TWO &&
+      (incomingGradeLevel === GradeLevel.GRADE_4 ||
+        incomingGradeLevel === GradeLevel.GRADE_5 ||
+        incomingGradeLevel === GradeLevel.GRADE_6)
+    ) {
+      gradeLevel = GradeLevel.GRADE_6;
+    }
+
+    return (
+      fee.accreditation === accreditation &&
+      fee.gradeLevel === gradeLevel &&
+      fee.type === enrollmentType
+    );
+  });
+
   return (
     <LandingLayout>
       <Meta title="Living Pupil Homeschool" />
       <Header {...headerSection} />
       <Title
-        title="Homeschool Cottage"
-        subtitle="Grade School - Senior High School"
+        title="Homeschool Program"
+        subtitle="Pre-school - Senior High School"
       />
+      <section className="px-5 py-10 space-y-10">
+        <div className="container mx-auto space-y-3">
+          <h3 className="text-lg font-bold text-center">
+            I'd like to enroll as a
+          </h3>
+          <div className="flex flex-row items-center justify-center space-x-3">
+            <button
+              className={`px-10 py-3 font-medium rounded-lg hover:text-white hover:bg-primary-500 border-2 border-primary-500 ${
+                enrollmentType === Enrollment.NEW && 'text-white bg-primary-500'
+              }`}
+              onClick={() => setEnrollmentType(Enrollment.NEW)}
+            >
+              New Family
+            </button>
+            <button
+              className={`px-10 py-3 font-medium rounded-lg hover:text-white hover:bg-primary-500 border-2 border-primary-500 ${
+                enrollmentType === Enrollment.CONTINUING &&
+                'text-white bg-primary-500'
+              }`}
+              onClick={() => setEnrollmentType(Enrollment.CONTINUING)}
+            >
+              Continuing Family
+            </button>
+          </div>
+        </div>
+        <div className="container mx-auto space-y-3">
+          <h3 className="text-lg font-bold text-center">with a</h3>
+          <div className="flex flex-row items-center justify-center space-x-3">
+            <button
+              className={`px-10 py-3 font-medium rounded-lg hover:text-white hover:bg-primary-500 border-2 border-primary-500 ${
+                accreditation === Accreditation.FORM_ONE &&
+                'text-white bg-primary-500'
+              }`}
+              onClick={() => {
+                setAccreditation(Accreditation.FORM_ONE);
+                setIncomingGradeLevel(GradeLevel.GRADE_1);
+              }}
+            >
+              Form 1
+            </button>
+            <button
+              className={`px-10 py-3 font-medium rounded-lg hover:text-white hover:bg-primary-500 border-2 border-primary-500 ${
+                accreditation === Accreditation.FORM_TWO &&
+                'text-white bg-primary-500'
+              }`}
+              onClick={() => {
+                setAccreditation(Accreditation.FORM_TWO);
+                setIncomingGradeLevel(GradeLevel.GRADE_4);
+              }}
+            >
+              Form 2
+            </button>
+          </div>
+        </div>
+        <div className="container w-1/4 mx-auto space-y-3">
+          <div className="relative inline-block w-full border rounded">
+            <select
+              className="w-full px-3 py-2 capitalize rounded appearance-none"
+              onChange={(e) => setIncomingGradeLevel(e.target.value)}
+              value={incomingGradeLevel}
+            >
+              {GRADE_LEVEL_GROUPS.map((group, index) => (
+                <optgroup key={index} label={group.name}>
+                  {group.levels.map((level, index) => (
+                    <option
+                      key={index}
+                      value={level}
+                      disabled={
+                        !(
+                          accreditation === Accreditation.FORM_ONE &&
+                          (level === GradeLevel.GRADE_1 ||
+                            level === GradeLevel.GRADE_2 ||
+                            level === GradeLevel.GRADE_3)
+                        ) &&
+                        !(
+                          accreditation === Accreditation.FORM_TWO &&
+                          (level === GradeLevel.GRADE_4 ||
+                            level === GradeLevel.GRADE_5 ||
+                            level === GradeLevel.GRADE_6)
+                        )
+                      }
+                    >
+                      {GRADE_LEVEL[level]}
+                    </option>
+                  ))}
+                </optgroup>
+              ))}
+            </select>
+            <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+              <ChevronDownIcon className="w-5 h-5" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="px-5 py-10">
+        <div className="container flex flex-row w-3/4 mx-auto space-x-5">
+          <div className="w-1/3 p-5 space-y-5 text-center border rounded-lg border-primary-500">
+            <h2 className="text-xl font-medium">Full Payment</h2>
+            <h3 className="text-3xl font-bold font-display">
+              {new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'PHP',
+              }).format(schoolFee?.fees[0]?.totalFee || 0)}
+            </h3>
+          </div>
+          <div className="w-1/3 p-5 space-y-5 text-center border rounded-lg border-primary-500">
+            <h2 className="text-xl font-medium">Semi-Annual Payment</h2>
+            <h3 className="text-3xl font-bold font-display">
+              {new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'PHP',
+              }).format(
+                schoolFee?.fees[1]?.initialFee +
+                  schoolFee?.fees[1]?.semiAnnualFee * 2 || 0
+              )}
+            </h3>
+          </div>
+          <div className="w-1/3 p-5 space-y-5 text-center border rounded-lg border-primary-500">
+            <h2 className="text-xl font-medium">Quarterly Payment</h2>
+            <h3 className="text-3xl font-bold font-display">
+              {new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'PHP',
+              }).format(
+                schoolFee?.fees[2]?.initialFee +
+                  schoolFee?.fees[2]?.quarterlyFee * 3 || 0
+              )}
+            </h3>
+          </div>
+        </div>
+      </section>
+      <section className="px-5 py-5 italic">
+        <p className="text-sm text-center">
+          {accreditation === Accreditation.FORM_ONE &&
+            enrollmentType === Enrollment.NEW && (
+              <>
+                <strong>Exclusion: </strong>
+                <span className="block">
+                  Graduation Fee, Recognition Day Fee Php 12,000.00 Additional
+                  Rate for International Accreditation
+                </span>
+              </>
+            )}
+          {/* {(accreditation === Accreditation.INTERNATIONAL ||
+            accreditation === Accreditation.DUAL) && (
+            <>
+              <strong>Note: </strong>
+              <span className="block">
+                Credits Review Fee Php 3,000.00 for Grade 11, Php 7,500.00 for
+                Grade 12
+              </span>
+              <span className="block">
+                Graduation Fee for Grade 12 only Php 5,000.00
+              </span>
+            </>
+          )} */}
+        </p>
+      </section>
       <Footer {...footerSection} />
     </LandingLayout>
   );
--- src/pages/homeschool-program.js
+++ src/pages/homeschool-program.js
@@ -1,14 +1,58 @@
+import { useState } from 'react';
+import { ChevronDownIcon } from '@heroicons/react/outline';
+import { Accreditation, Enrollment, GradeLevel } from '@prisma/client';
+
 import Meta from '@/components/Meta/index';
 import { LandingLayout } from '@/layouts/index';
 import sanityClient from '@/lib/server/sanity';
 import Footer from '@/sections/footer';
 import Header from '@/sections/header';
 import Title from '@/sections/sectionTitle';
+import { GRADE_LEVEL, GRADE_LEVEL_GROUPS } from '@/utils/constants';
 
 const HomeschoolProgram = ({ page, fees }) => {
   const { footer, header } = page;
   const [headerSection] = header?.sectionType;
   const [footerSection] = footer?.sectionType;
+  const [enrollmentType, setEnrollmentType] = useState(Enrollment.NEW);
+  const [incomingGradeLevel, setIncomingGradeLevel] = useState(
+    GradeLevel.PRESCHOOL
+  );
+  const [accreditation, setAccreditation] = useState(Accreditation.LOCAL);
+
+  const schoolFee = fees.schoolFees.find((fee) => {
+    let gradeLevel = incomingGradeLevel;
+
+    if (
+      incomingGradeLevel === GradeLevel.GRADE_1 ||
+      incomingGradeLevel === GradeLevel.GRADE_2 ||
+      incomingGradeLevel === GradeLevel.GRADE_3 ||
+      incomingGradeLevel === GradeLevel.GRADE_4 ||
+      incomingGradeLevel === GradeLevel.GRADE_5 ||
+      incomingGradeLevel === GradeLevel.GRADE_6
+    ) {
+      gradeLevel = GradeLevel.GRADE_6;
+    } else if (
+      incomingGradeLevel === GradeLevel.GRADE_7 ||
+      incomingGradeLevel === GradeLevel.GRADE_8 ||
+      incomingGradeLevel === GradeLevel.GRADE_9 ||
+      incomingGradeLevel === GradeLevel.GRADE_10
+    ) {
+      gradeLevel = GradeLevel.GRADE_10;
+    } else if (
+      incomingGradeLevel === GradeLevel.GRADE_11 ||
+      incomingGradeLevel === GradeLevel.GRADE_12
+    ) {
+      gradeLevel = GradeLevel.GRADE_12;
+    }
+
+    return (
+      fee.accreditation === accreditation &&
+      fee.gradeLevel === gradeLevel &&
+      fee.type === enrollmentType
+    );
+  });
+
   return (
     <LandingLayout>
       <Meta title="Living Pupil Homeschool" />
@@ -17,6 +61,165 @@ const HomeschoolProgram = ({ page, fees }) => {
         title="Homeschool Program"
         subtitle="Pre-school - Senior High School"
       />
+      <section className="px-5 py-10 space-y-10">
+        <div className="container mx-auto space-y-3">
+          <h3 className="text-lg font-bold text-center">
+            I'd like to enroll as a
+          </h3>
+          <div className="flex flex-row items-center justify-center space-x-3">
+            <button
+              className={`px-10 py-3 font-medium rounded-lg hover:text-white hover:bg-primary-500 border-2 border-primary-500 ${
+                enrollmentType === Enrollment.NEW && 'text-white bg-primary-500'
+              }`}
+              onClick={() => setEnrollmentType(Enrollment.NEW)}
+            >
+              New Family
+            </button>
+            <button
+              className={`px-10 py-3 font-medium rounded-lg hover:text-white hover:bg-primary-500 border-2 border-primary-500 ${
+                enrollmentType === Enrollment.CONTINUING &&
+                'text-white bg-primary-500'
+              }`}
+              onClick={() => setEnrollmentType(Enrollment.CONTINUING)}
+            >
+              Continuing Family
+            </button>
+          </div>
+        </div>
+        <div className="container mx-auto space-y-3">
+          <h3 className="text-lg font-bold text-center">with a</h3>
+          <div className="flex flex-row items-center justify-center space-x-3">
+            <button
+              className={`px-10 py-3 font-medium rounded-lg hover:text-white hover:bg-primary-500 border-2 border-primary-500 ${
+                accreditation === Accreditation.LOCAL &&
+                'text-white bg-primary-500'
+              }`}
+              onClick={() => setAccreditation(Accreditation.LOCAL)}
+            >
+              Local Accreditation
+            </button>
+            <button
+              className={`px-10 py-3 font-medium rounded-lg hover:text-white hover:bg-primary-500 border-2 border-primary-500 ${
+                accreditation === Accreditation.INTERNATIONAL &&
+                'text-white bg-primary-500'
+              }`}
+              onClick={() => {
+                setAccreditation(Accreditation.INTERNATIONAL);
+                setIncomingGradeLevel(GradeLevel.K2);
+              }}
+            >
+              International Accreditation
+            </button>
+            <button
+              className={`px-10 py-3 font-medium rounded-lg hover:text-white hover:bg-primary-500 border-2 border-primary-500 ${
+                accreditation === Accreditation.DUAL &&
+                'text-white bg-primary-500'
+              }`}
+              onClick={() => {
+                setAccreditation(Accreditation.DUAL);
+                setIncomingGradeLevel(GradeLevel.K2);
+              }}
+            >
+              Dual Accreditation
+            </button>
+          </div>
+        </div>
+        <div className="container w-1/4 mx-auto space-y-3">
+          <div className="relative inline-block w-full border rounded">
+            <select
+              className="w-full px-3 py-2 capitalize rounded appearance-none"
+              onChange={(e) => setIncomingGradeLevel(e.target.value)}
+              value={incomingGradeLevel}
+            >
+              {GRADE_LEVEL_GROUPS.map((group, index) => (
+                <optgroup key={index} label={group.name}>
+                  {group.levels.map((level, index) => (
+                    <option
+                      key={index}
+                      value={level}
+                      disabled={
+                        (accreditation === Accreditation.INTERNATIONAL ||
+                          accreditation === Accreditation.DUAL) &&
+                        (level === GradeLevel.PRESCHOOL ||
+                          level === GradeLevel.K1)
+                      }
+                    >
+                      {GRADE_LEVEL[level]}
+                    </option>
+                  ))}
+                </optgroup>
+              ))}
+            </select>
+            <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+              <ChevronDownIcon className="w-5 h-5" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="px-5 py-10">
+        <div className="container flex flex-row w-3/4 mx-auto space-x-5">
+          <div className="w-1/3 p-5 space-y-5 text-center border rounded-lg border-primary-500">
+            <h2 className="text-xl font-medium">Full Payment</h2>
+            <h3 className="text-3xl font-bold font-display">
+              {new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'PHP',
+              }).format(schoolFee?.fees[0]?.totalFee || 0)}
+            </h3>
+          </div>
+          <div className="w-1/3 p-5 space-y-5 text-center border rounded-lg border-primary-500">
+            <h2 className="text-xl font-medium">Semi-Annual Payment</h2>
+            <h3 className="text-3xl font-bold font-display">
+              {new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'PHP',
+              }).format(
+                schoolFee?.fees[1]?.initialFee +
+                  schoolFee?.fees[1]?.semiAnnualFee * 2 || 0
+              )}
+            </h3>
+          </div>
+          <div className="w-1/3 p-5 space-y-5 text-center border rounded-lg border-primary-500">
+            <h2 className="text-xl font-medium">Quarterly Payment</h2>
+            <h3 className="text-3xl font-bold font-display">
+              {new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'PHP',
+              }).format(
+                schoolFee?.fees[2]?.initialFee +
+                  schoolFee?.fees[2]?.quarterlyFee * 3 || 0
+              )}
+            </h3>
+          </div>
+        </div>
+      </section>
+      <section className="px-5 py-5 italic">
+        <p className="text-sm text-center">
+          {accreditation === Accreditation.LOCAL &&
+            enrollmentType === Enrollment.CONTINUING && (
+              <>
+                <strong>Note: </strong>
+                <span className="block">
+                  Sibling discount is Php 1,000.00 for the second, third, and
+                  fourth child
+                </span>
+              </>
+            )}
+          {(accreditation === Accreditation.INTERNATIONAL ||
+            accreditation === Accreditation.DUAL) && (
+            <>
+              <strong>Note: </strong>
+              <span className="block">
+                Credits Review Fee Php 3,000.00 for Grade 11, Php 7,500.00 for
+                Grade 12
+              </span>
+              <span className="block">
+                Graduation Fee for Grade 12 only Php 5,000.00
+              </span>
+            </>
+          )}
+        </p>
+      </section>
       <Footer {...footerSection} />
     </LandingLayout>
   );
