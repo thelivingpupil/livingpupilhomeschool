0c1f6305169d7dc7be9c1eb1096f393e746c55a8
Rouie Ilustrisimo <ilustrisimo.rouie@outlook.com>
2022-12-29 12:39:52 +0800

    transactions

--- src/hooks/data/useTransactions.js
+++ src/hooks/data/useTransactions.js
@@ -4,12 +4,8 @@ const useTransactions = () => {
   const apiRoute = `/api/transactions`;
   const { data, error } = useSWR(`${apiRoute}`);
 
-  const sortedItems = data.sort((a, b) =>
-    b.createdAt.localeCompare(a.createdAt)
-  );
-
   return {
-    ...sortedItems,
+    ...data,
     isLoading: !error && !data,
     isError: error,
   };
