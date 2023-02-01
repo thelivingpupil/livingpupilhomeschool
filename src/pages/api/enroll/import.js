c8f613dd7ab3c942320e108c7f52352fa31f27a7
Gideon Arces <gid.arces07@gmail.com>
2023-02-01 15:11:23 +0800

    Clean-up on import api

--- src/pages/api/enroll/import.js
+++ src/pages/api/enroll/import.js
@@ -2,28 +2,31 @@ import { validateSession } from "@/config/api-validation";
 import prisma from "@/prisma/index";
 
 const handler = async (req, res) => {
- const session = await validateSession(req, res);
+ try {
+  const session = await validateSession(req, res);
 
- const user = await prisma.user.findUnique({
-    select: {
-      id: true,
-      email: true,
-      name: true,
-      userCode: true,
-    },
-    where: { email: 'babydaughson@gmail.com' },
-  });
+  const user = await prisma.user.findUnique({
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        userCode: true,
+      },
+      where: { email: 'babydaughson@gmail.com' },
+    });
 
-  // console.log('I am here');
+    console.log('I am here');
 
-  // console.log(JSON.stringify(user));
+    console.log(JSON.stringify(user));
 
-  // console.log(JSON.stringify());
+    console.log(JSON.stringify());
 
-  res.status(200).json({
-    user,
-    session
-  });
+    res.status(200).json({ message: 'Successful import' });
+ } catch (error) {
+    res.status(400).json({
+      error
+    })
+ }
 }
 
 export default handler;
