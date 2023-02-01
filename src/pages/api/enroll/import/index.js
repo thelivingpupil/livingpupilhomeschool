86dadb5ee830d0cfc3d3148be268db4c51b0dff8
Gideon Arces <gid.arces07@gmail.com>
2023-02-01 16:30:27 +0800

    Adding workspace query

--- src/pages/api/enroll/import/index.js
+++ src/pages/api/enroll/import/index.js
@@ -3,25 +3,23 @@ import prisma from "@/prisma/index";
 
 const handler = async (req, res) => {
  try {
-  const session = await validateSession(req, res);
+    const session = await validateSession(req, res);
 
-  const user = await prisma.user.findUnique({
+    const user = await prisma.user.findUnique({
       select: {
         id: true,
         email: true,
         name: true,
         userCode: true,
+        createdWorkspace: true
       },
       where: { email: 'babydaughson@gmail.com' },
     });
 
-    console.log('I am here');
-
-    console.log(JSON.stringify(user));
-
-    console.log(JSON.stringify());
-
-    res.status(200).json({ message: 'Successful import' });
+    res.status(200).json({ message: 'Successful import', data: {
+      session,
+      user
+    } });
  } catch (error) {
     res.status(400).json({
       error
