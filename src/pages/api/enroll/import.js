6f11e83b9656fb59e5134e2b6896d0956e3b6d75
Gideon Arces <gid.arces07@gmail.com>
2023-02-02 15:55:40 +0800

    Incapsulate processUser inside handler

--- src/pages/api/enroll/import.js
+++ src/pages/api/enroll/import.js
@@ -1,60 +1,59 @@
 import { validateSession } from "@/config/api-validation";
 import prisma from "@/prisma/index";
 
-const processUser = async ({email, firstName}) => {
-  const session = await validateSession(req, res);
-
-    const user = await prisma.user.findUnique({
-      select: {
-        id: true,
-        email: true,
-        name: true,
-        userCode: true,
-        createdWorkspace: true
-      },
-      where: { email },
-    });
-
-    const activeUser = user ?? await prisma.user.findUnique({
-      select: {
-        id: true,
-        email: true,
-        name: true,
-        userCode: true,
-        createdWorkspace: true
-      },
-      where: { id: session.user.userId },
-    });
-
-    const existingWorkspace = activeUser.createdWorkspace.find((workspace) => {
-      const validation = workspace.name.length > firstName.length ? {
-        compareWith: workspace.name.toLowerCase(),
-        compareTo: firstName.toLowerCase()
-      } : {
-        compareWith: firstName.toLowerCase(),
-        compareTo: workspace.name.toLowerCase()
-      }
-
-      return validation.compareWith.includes(validation.compareTo)
-    })
-
-    const workspace = existingWorkspace ?? 'test';
-
-    return {
-      session,
-      user,
-      activeUser,
-      existingWorkspace,
-      workspace
-    }
-}
-
 const handler = async (req, res) => {
  try {
     const firstName = 'KHLOE COLLEEN';
+    const email = 'babydaughson@gmail.com'
+    const processUser = async ({email, firstName }) => {
+      const session = await validateSession(req, res);
 
-    const data = await processUser({email: 'babydaughson@gmail.com', firstName})
+      const user = await prisma.user.findUnique({
+        select: {
+          id: true,
+          email: true,
+          name: true,
+          userCode: true,
+          createdWorkspace: true
+        },
+        where: { email },
+      });
 
+      const activeUser = user ?? await prisma.user.findUnique({
+        select: {
+          id: true,
+          email: true,
+          name: true,
+          userCode: true,
+          createdWorkspace: true
+        },
+        where: { id: session.user.userId },
+      });
+
+      const existingWorkspace = activeUser.createdWorkspace.find((workspace) => {
+        const validation = workspace.name.length > firstName.length ? {
+          compareWith: workspace.name.toLowerCase(),
+          compareTo: firstName.toLowerCase()
+        } : {
+          compareWith: firstName.toLowerCase(),
+          compareTo: workspace.name.toLowerCase()
+        }
+
+        return validation.compareWith.includes(validation.compareTo)
+      })
+
+      const workspace = existingWorkspace ?? 'test';
+
+      return {
+        session,
+        user,
+        activeUser,
+        existingWorkspace,
+        workspace
+      }
+    }
+
+    const data = await processUser({email, firstName});
 
     res.status(200).json({ message: 'Successful import', data });
  } catch (error) {
