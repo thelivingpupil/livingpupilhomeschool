59aab0658f96d2fd7b34ae4d30ac478241a86781
Gideon Arces <gid.arces07@gmail.com>
2023-02-02 15:34:01 +0800

    Adding activeUser for import

--- src/pages/api/enroll/import.js
+++ src/pages/api/enroll/import.js
@@ -17,9 +17,21 @@ const handler = async (req, res) => {
       where: { email: 'babydaughson@gmail.com' },
     });
 
+    const activeUser = user ?? await prisma.user.findUnique({
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        userCode: true,
+        createdWorkspace: true
+      },
+      where: { id: session.user.userId },
+    });
+
     res.status(200).json({ message: 'Successful import', data: {
       session,
       user,
+      activeUser
     } });
  } catch (error) {
     res.status(400).json({
