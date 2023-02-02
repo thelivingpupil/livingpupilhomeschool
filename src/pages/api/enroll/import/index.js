038d4c924f35deb5cb067102b7444ce944996392
Gideon Arces <gid.arces07@gmail.com>
2023-02-02 15:17:19 +0800

    Adding code to import handler

--- src/pages/api/enroll/import/index.js
+++ src/pages/api/enroll/import/index.js
@@ -1,10 +1,35 @@
-import { processUser } from "./processUser";
-
 const handler = async (req, res) => {
  try {
-    const data = await processUser({email: 'babydaughson@gmail.com', firstName: 'KHLOE COLLEEN '})
 
-    res.status(200).json({ message: 'Successful import', data });
+    const session = await validateSession(req, res);
+
+    const user = await prisma.user.findUnique({
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        userCode: true,
+        createdWorkspace: true
+      },
+      where: { email: 'babydaughson@gmail.com' },
+    });
+
+    const activeUser = user || await prisma.user.findUnique({
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        userCode: true,
+        createdWorkspace: true
+      },
+      where: { id: session.user.userId },
+    });
+
+    res.status(200).json({ message: 'Successful import', data: {
+      session,
+      user,
+      activeUser
+    } });
  } catch (error) {
     res.status(400).json({
       error
