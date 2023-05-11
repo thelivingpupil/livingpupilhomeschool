9f2700670cd8ceb9931db78e60c47b675a48fdfb
Gideon Arces <gid.arces07@gmail.com>
2023-05-11 17:06:21 +0800

    Adding css for card

--- src/pages/account/community.js
+++ src/pages/account/community.js
@@ -31,10 +31,10 @@ const Community = () => {
         </Card>
       </Content.Container>
 
-      <div className="flex flex-row md:flex-col">
+      <div className="flex flex-row xs:flex-col space-5">
         <div className="flex items-end">
-          <div className="flex items-end h-3/4 xs:h-full">
-            <div className="flex flex-col sm:flex-row bg-water-500 pt-28">
+          <div className="flex items-end h-3/4 xs:h-full rounded-md">
+            <div className="flex flex-col xs:flex-row bg-water-500 pt-28">
               <Image
                 alt="Living Pupil Homeschool"
                 src="/images/livingpupil-homeschool-logo.png"
