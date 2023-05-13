fb2a36aa5cfc982021e9cbfcd8c44a663b4c28ce
Gideon Arces <gid.arces07@gmail.com>
2023-05-13 14:45:07 +0800

    Adding container for card

--- src/pages/account/community.js
+++ src/pages/account/community.js
@@ -48,14 +48,16 @@ const Community = () => {
           </div>
         </div>
         <div className="grid grid-cols-3 space-x-5 space-y-5">
-          <div className="flex flex-end">
-            <div className="flex flex-col xs:flex-row bg-water-700 p-8 items-center rounded-xl mt-5 ml-5">
-              <div className="flex bg-water-100">
-                <FaFacebook size={100} color="#0165E1" />
+          <div className="flex flex-1">
+            <div className="flex items-end">
+              <div className="flex flex-col xs:flex-row bg-water-700 p-8 items-center rounded-xl mt-5 ml-5">
+                <div className="flex bg-water-100">
+                  <FaFacebook size={100} color="#0165E1" />
+                </div>
+                <span className="text-lg text-primary-500 font-semibold p-2 max-w-[150px] text-center">
+                  Living Pupil Facebook Page
+                </span>
               </div>
-              <span className="text-lg text-primary-500 font-semibold p-2 max-w-[150px] text-center">
-                Living Pupil Facebook Page
-              </span>
             </div>
           </div>
           <div className="flex flex-end">
