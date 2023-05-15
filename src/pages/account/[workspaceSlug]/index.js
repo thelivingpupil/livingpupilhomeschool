010653c2c28655df3319731d8ee8663a15444b13
Gideon Arces <gid.arces07@gmail.com>
2023-05-15 16:09:13 +0800

    Updating Policies for Enrollment

--- src/pages/account/[workspaceSlug]/index.js
+++ src/pages/account/[workspaceSlug]/index.js
@@ -1578,51 +1578,95 @@ const Workspace = ({ guardian, schoolFees, programs }) => {
           </h3>
           <ol className="px-5 list-decimal">
             <li>
-              Parents who intend to enroll are required to watch our “HOMESCHOOL
+              Parents who intend to enroll are required to watch our "HOMESCHOOL
               FUNDAMENTALS" videos.
             </li>
             <li>
               Should the parent choose to use Pure Charlotte Mason or A CM
-              inspired Curriculum, the parent must attend the CM
+              inspired Curriculum, the parents must attend the CM
               training/workshop faithfully.
             </li>
             <li>
-              It is expected of the parents to dutifully fulfill the annual, bi
-              annual or quarterly dues and other financial obligation of their
-              children on time.
+              It is expected of the parents to fulfill the annual, bi-annual
+              dutifully or Quarterly dues and other financial obligations of
+              their children on time.
             </li>
           </ol>
-          <p>Payment Policies:</p>
+          <p>Parents understand that:</p>
           <ol className="px-5 list-disc">
             <li>
               A child is considered fully enrolled only after full payment of
               the annual homeschool fee.
             </li>
           </ol>
-          <h3 className="text-sm font-bold">
-            Student's Duties and Expectations
-          </h3>
-          <p>We understand that:</p>
-          <ol className="px-5 list-decimal">
+          <p>
+            As a parent-teacher, I acknowledge that it is my responsibility to:
+          </p>
+          <ol className="px-5 list-disc">
             <li>
-              The student is required and expected to join the Bi Annual
-              portfolio presentation and other coop class/meet ups when the
-              health and safety of students are no longer at risk due to COVID.
-              Students outside Cebu City is expected to join the virtually.
+              Commit time to plan, study, and organize our homeschool
+              curriculum.
             </li>
             <li>
-              The student (residing in Cebu/Cebu City) is required to
-              participate in every Commencement Exercises (only when COVID
-              pandemic is already flattened and the Government declares it is
-              now safe to do so) organized by the school with corresponding
-              fees. Graduation fees are non-refundable.
+              Intentionally attend/watch the self-paced training and the live LP
+              parent-teacher training.
             </li>
             <li>
-              In instances wherein a student is directly negligent with his
-              academic responsibilities, an immediate conference with the
-              child’s parents will be secured. It is the responsibility of the
-              parent to attend to this meeting so that an immediate solution to
-              the challenge will be agreed upon.
+              Dedicate time to attend/participate in the scheduled coaching
+              sessions with the LP homeschool advisers/coach whenever available.
+            </li>
+            <li>
+              Administer quarterly assessments (using full CM or CM-inspired
+              approach).
+            </li>
+            <li>
+              Submit quarterly grades and final grades (with average) at the end
+              of the school year using the instructions provided by Living Pupil
+              Homeschool. (For K2 to Grade 10 students only).
+            </li>
+            <li>
+              Make the school aware of any concerns or problems that might
+              affect my/our child’s academic work or behavior.
+            </li>
+            <li>
+              Comply with DepEd’s number of school days per school year
+              requirement. (at least 205 school days and follow the DepEd
+              calendar in terms of school records).
+            </li>
+          </ol>
+          <p>
+            As part of the Living Pupil family, I acknowledge that it is my
+            responsibility to:
+          </p>
+          <ol className="px-5 list-disc">
+            <li>
+              Support and encourage Living Pupil Homeschool’s values that are
+              central to the philosophy and mission of Charlotte Mason.
+            </li>
+            <li>
+              Make myself aware of the latest LP advisory and announcements by
+              regularly checking my emails, Living Pupil Facebook Page, Living
+              Pupil FB groups/Living Pupil FB chat room, and other communication
+              platforms used by the school.
+            </li>
+            <li>
+              Responsibly fulfill my financial obligation to the school on time.
+            </li>
+            <li>
+              Understand the importance of being present during LP events and
+              activities.
+            </li>
+            <li>
+              Permit to use photographs/videos of me and my family and
+              information obtained through personal interviews in any of Living
+              Pupil’s publications, social media platforms, marketing materials,
+              or virtual events.
+            </li>
+            <li>
+              Agree not to reproduce, distribute or display (post/upload)
+              lecture notes or parent training recordings or course materials in
+              any other way — whether or not a fee is charged — without my
+              express written consent.
             </li>
           </ol>
         </div>
@@ -1867,31 +1911,25 @@ const Workspace = ({ guardian, schoolFees, programs }) => {
             <h3 className="text-sm font-bold">Payment Policies:</h3>
             <ol className="px-5 list-decimal">
               <li>
+                ALL payment transactions must be processed via our payment
+                partner, dragon pay (online link provided by our finance or LP
+                website). This is to ensure a safe, real-time one-day processing
+                and posting of successful transactions. This means no payment
+                proof submission is required from you, and there is no need to
+                input our LP bank details upon payment. Payments NOT MADE
+                through this channel may not be recognized or recorded.
+              </li>
+              <li>
                 A 3% interest will be added to the school fee when the parents
                 are not able to pay on time.
               </li>
               <li>
                 For Post-dated checks, bounced or delayed payments will be
-                subjected to 5% interest per month.
-              </li>
-              <li>
-                It is expected of the Parents to submit every quarter the grades
-                and assessment, class documentation (at least 2 photos/videos
-                per subject per quarter) and eCard page 2 (number of school
-                days, traits and teacher&apos;s comments form). At the end of
-                the school year, parents should compile in a digital form all
-                the outputs per subject and send /share the drive link to LPHS
-                admin. Deadline of submission: Q1 & Q2 = January 10, 2023 and Q3
-                & Q4 = June 8, 2023. Failure to submit grades will incur a
-                retard fee of 1,000.00 per delay.
-              </li>
-              <li>
-                Students are expected to present an Annual portfolio (Show and
-                Tell).
+                subjected to 5% monthly interest.
               </li>
               <li>
                 All school records will be released after the student has been
-                cleared of financial and academic obligations from the LPHS.
+                cleared of financial and academic obligations from LPHS.
               </li>
               <li>
                 In case of non-compliance with LPHS requirements and/or the
@@ -1900,55 +1938,69 @@ const Workspace = ({ guardian, schoolFees, programs }) => {
                 year.
               </li>
               <li>
-                Living Pupil Homeschool&apos;s foundation is build on trust and
-                relationship with our families. We therefore require our
+                Living Pupil Homeschool's foundation is built on trust and
+                relationships with our families. We, therefore, require our
                 Parent-Teachers to do their task with honesty & commitment.
                 Should there be homeschool concerns, please communicate directly
-                to our team.
+                with our team.
               </li>
             </ol>
             <h3 className="text-sm font-bold">
               Refund on Tuition Fees and other fees:
             </h3>
-            <p>
-              A student who wishes to transfer or withdraw his/her enrollment
-              from LPHS would be entitled to a tuition refund provided: (1)
-              he/she submit a letter of withdrawal/transfer (2) has already paid
-              the tuition and other fees in full. The amount of refund depends
-              on:
-            </p>
             <ol className="px-5 list-decimal">
               <li>
-                When the student withdraws a week after enrolment, 10% of the
-                total annual due will be charged.
+                The down payment upon enrollment is a nonrefundable deposit.
               </li>
               <li>
-                When the student withdraws two (2) weeks after enrollment, 20%
-                of the total annual due will be charged.
+                In the event that the parent wishes to transfer or withdraw
+                their child's enrollment from LPHS and they paid the tuition fee
+                in full or an amount greater than the required down payment, the
+                parent is entitled to a tuition refund provided: (1) they submit
+                a letter of withdrawal/transfer (2) has already paid the tuition
+                and other fees in full. The amount of the refund depends on the
+                following:
+                <ul>
+                  <li>
+                    When the student withdraws a week after enrollment, LP
+                    finance will charge 10% of the amount paid plus the
+                    nonrefundable down payment.
+                  </li>
+                  <li>
+                    When the student withdraws two (2) weeks after enrollment,
+                    LP finance will charge 20% of the amount paid plus the
+                    nonrefundable down payment.
+                  </li>
+                  <li>
+                    No refund will be given when the student withdraws one (1)
+                    month after enrollment.
+                  </li>
+                  <li>
+                    There will be no refund for books and other learning
+                    materials.
+                  </li>
+                </ul>
               </li>
               <li>
-                When the student withdraws one (1) one month after enrollment,
-                no refund will be given.
-              </li>
-              <li>
-                There will be no refund for books and other learning materials.
-              </li>
-              <li>
-                When the student withdraw before the school year ends, the
+                When the student withdraws before the school year ends, the
                 parent will pay the annual tuition fee for the school records to
                 be released.
+              </li>
+              <li>
+                All students are required to participate in the year-end
+                PAGSAULOG (Graduation, Moving-up, and Recognition Celebration)
+                either online or face-to-face and are subject to a registration
+                fee of P1300 - P2500 per family.
               </li>
             </ol>
           </div>
           <div>
             <p>
-              By completing and submitting this form, I understand that I am
-              applying to be a part of Living Pupil Homeschool for SY
-              {new Date().getFullYear()} - {new Date().getFullYear() + 1}. I
-              have thoroughly reviewed the forms, and I agree with everything
-              stipulated in the student form. I agree to the rates as indicated
-              for this enrollment period. Lastly, I understand that this
-              agreement is absolute and will be enforced.
+              By completing and submitting this form, we agree to follow the
+              terms listed above in the Living Pupil Homeschool Agreement. We
+              understand that this agreement is bona fide and will be enforced.
+              Living Pupil Homeschool reserves the right to amend this
+              Agreement.
             </p>
             <hr className="my-5 border border-dashed" />
             <div className="flex flex-col mt-5 space-x-0 space-y-5 md:flex-row md:justify-between md:space-x-5 md:space-y-0">
