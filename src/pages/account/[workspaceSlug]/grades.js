18685b479d9a26cb7b03752edaf45bb7d5e2012e
Gideon Arces <gid.arces07@gmail.com>
2023-05-19 15:54:49 +0800

    Adding forms for grade level

--- src/pages/account/[workspaceSlug]/grades.js
+++ src/pages/account/[workspaceSlug]/grades.js
@@ -4,6 +4,21 @@ import { AccountLayout } from '@/layouts/index';
 import Card from '@/components/Card';
 import { useWorkspace } from '@/providers/workspace';
 import JotFormEmbed from 'react-jotform-embed';
+import { GradeLevel } from '@prisma/client';
+
+const forms = {
+  [GradeLevel.K2]: '231091044111438',
+  [GradeLevel.GRADE_1]: '230861217347455',
+  [GradeLevel.GRADE_2]: '230861217347455',
+  [GradeLevel.GRADE_3]: '230861217347455',
+  [GradeLevel.GRADE_4]: '231090829906460',
+  [GradeLevel.GRADE_5]: '231090829906460',
+  [GradeLevel.GRADE_6]: '231090829906460',
+  [GradeLevel.GRADE_7]: '231090829906460',
+  [GradeLevel.GRADE_8]: '231090829906460',
+  [GradeLevel.GRADE_9]: '231090829906460',
+  [GradeLevel.GRADE_10]: '231090829906460',
+};
 
 const Grades = () => {
   const { workspace } = useWorkspace();
@@ -17,25 +32,23 @@ const Grades = () => {
         subtitle="View your student's grades"
       />
       <Content.Divider />
-      <Content.Container>
-        <Card>
-          <Card.Body
-            title="This is where you can manage your student's grades"
-            subtitle="You may visit our Facebook page for more details and updates"
-          >
-            <a
-              className="w-full py-2 text-center rounded-lg text-primary-500 bg-secondary-500 hover:bg-secondary-600 disabled:opacity-25"
-              href="https://www.facebook.com/livingpupilhomeschool"
-              target="_blank"
-            >
-              Visit Facebook Page
-            </a>
-          </Card.Body>
-        </Card>
-      </Content.Container>
-      <JotFormEmbed src="https://form.jotform.com/230861217347455" />
+
+      {workspace &&
+        workspace?.studentRecord?.incomingGradeLevel &&
+        forms[workspace?.studentRecord?.incomingGradeLevel] && (
+          <JotFormEmbed
+            src={`https://form.jotform.com/${
+              forms[workspace?.studentRecord?.incomingGradeLevel]
+            }`}
+            scrolling={true}
+          />
+        )}
     </AccountLayout>
   );
 };
 
 export default Grades;
+// "log ins: (Grades 1-3) admthelivingpupil@gmail.com Kaz52020 https://form.jotform.com/230861217347455
+// (grades 4) thelivingpupil@gmail.com courage1 https://form.jotform.com/231090829906460
+// (grades 5-10) invoicethelivingpupil@gmail.com cephas123 https://form.jotform.com/231091154537452
+// K2 Jotform LPwebsite2022@gmail.com Arjeli2022!" https://form.jotform.com/231091044111438
