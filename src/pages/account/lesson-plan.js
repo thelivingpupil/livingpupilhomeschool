a9cfdbbe39652bae4014fbfd52e3e013a114e538
Gideon Arces <gid.arces07@gmail.com>
2023-05-21 19:32:51 +0800

    Fixing issue on download button width

--- src/pages/account/lesson-plan.js
+++ src/pages/account/lesson-plan.js
@@ -54,7 +54,7 @@ const LessonPlan = ({ lessonPlans }) => {
                   return (
                     <div key={idx} className="flex justify-center">
                       <a
-                        className={`flex items-center justify-center py-2 px-3 rounded ${bgColor}-600 text-white w-2/3 text-sm cursor-pointer hover:${bgColor}-500`}
+                        className={`flex items-center justify-center py-2 px-3 rounded ${bgColor}-600 text-white w-full md:w-4/5 text-sm cursor-pointer hover:${bgColor}-500`}
                         href={`${
                           plan?.fileUrl
                         }?dl=${plan?.grade?.toLowerCase()}-lesson_plan.pdf`}
