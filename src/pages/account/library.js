42bfa2cb6176c314bce5b2afd281e6fa216557ec
Gideon Arces <gid.arces07@gmail.com>
2023-05-21 16:17:38 +0800

    Fixing UI for virtual library

--- src/pages/account/library.js
+++ src/pages/account/library.js
@@ -1,3 +1,4 @@
+import React, { useMemo } from 'react';
 import Content from '@/components/Content/index';
 import imageUrlBuilder from '@sanity/image-url';
 import Meta from '@/components/Meta';
@@ -11,9 +12,16 @@ const imageBuilder = imageUrlBuilder(sanityClient);
 
 const Library = ({ books }) => {
   console.log('books', books);
-  const book = books && books[0];
-  const imageAsset = imageBuilder.image(book?.cover?.asset);
-  const image = imageAsset?.options?.source ? imageAsset?.url() : null;
+
+  const parentBooks = useMemo(
+    () => books?.filter((book) => book.for === 'PARENTS'),
+    [books]
+  );
+
+  const studentBooks = useMemo(
+    () => books?.filter((book) => book.for === 'STUDENTS'),
+    [books]
+  );
   return (
     <AccountLayout>
       <Meta title="Living Pupil Homeschool - Virtual Library" />
@@ -23,204 +31,114 @@ const Library = ({ books }) => {
       />
       <Content.Divider />
       <Content.Container>
-        <div className="flex flex-col">
-          <div className="flex justify-between">
-            <div>Virtual Library</div>
+        <div className="flex flex-col space-y-10 px-5 py-10 rounded border border-gray-500">
+          <div className="flex justify-between items-center">
+            <div className="text-2xl font-bold">Virtual Library</div>
             <div className="flex flex-end">
-              <div>options</div>
-              <div>search</div>
+              <input
+                className="px-3 py-2 border rounded"
+                placeholder="Search..."
+              />
             </div>
           </div>
-          {books?.length > 0 && (
-            <div className="flex flex-col">
-              <div>Parents</div>
-              <div>
-                <Carousel cols={5} rows={1} gap={10} loop>
-                  <Carousel.Item>
-                    <div className="flex flex-col justify-center items-center space-y-3 p-2">
-                      <div>
-                        <Image
-                          alt={slugify(book?.title)}
-                          src={
-                            image || '/images/livingpupil-homeschool-logo.png'
-                          }
-                          width={150}
-                          height={250}
-                        />
-                      </div>
-                      <a
-                        className="flex items-center justify-center py-2 px-3 rounded bg-primary-600 text-white w-2/3 text-sm cursor-pointer hover:bg-primary-500"
-                        href={`${book?.file}?dl=${slugify(book?.title)}.pdf`}
-                      >
-                        Download
-                      </a>
-                    </div>
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <div className="flex flex-col justify-center items-center space-y-3 p-2">
-                      <div>
-                        <Image
-                          alt={slugify(book?.title)}
-                          src={
-                            image || '/images/livingpupil-homeschool-logo.png'
-                          }
-                          width={150}
-                          height={250}
-                        />
-                      </div>
-                      <a
-                        className="flex items-center justify-center py-2 px-3 rounded bg-primary-600 text-white w-2/3 text-sm cursor-pointer hover:bg-primary-500"
-                        href={`${book?.file}?dl=${slugify(book?.title)}.pdf`}
-                      >
-                        Download
-                      </a>
-                    </div>
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <div className="flex flex-col justify-center items-center space-y-3 p-2">
-                      <div>
-                        <Image
-                          alt={slugify(book?.title)}
-                          src={
-                            image || '/images/livingpupil-homeschool-logo.png'
-                          }
-                          width={150}
-                          height={250}
-                        />
-                      </div>
-                      <a
-                        className="flex items-center justify-center py-2 px-3 rounded bg-primary-600 text-white w-2/3 text-sm cursor-pointer hover:bg-primary-500"
-                        href={`${book?.file}?dl=${slugify(book?.title)}.pdf`}
-                      >
-                        Download
-                      </a>
-                    </div>
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <div className="flex flex-col justify-center items-center space-y-3 p-2">
-                      <div>
-                        <Image
-                          alt={slugify(book?.title)}
-                          src={
-                            image || '/images/livingpupil-homeschool-logo.png'
-                          }
-                          width={150}
-                          height={250}
-                        />
-                      </div>
-                      <a
-                        className="flex items-center justify-center py-2 px-3 rounded bg-primary-600 text-white w-2/3 text-sm cursor-pointer hover:bg-primary-500"
-                        href={`${book?.file}?dl=${slugify(book?.title)}.pdf`}
-                      >
-                        Download
-                      </a>
-                    </div>
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <div className="flex flex-col justify-center items-center space-y-3 p-2">
-                      <div>
-                        <Image
-                          alt={slugify(book?.title)}
-                          src={
-                            image || '/images/livingpupil-homeschool-logo.png'
-                          }
-                          width={150}
-                          height={250}
-                        />
-                      </div>
-                      <a
-                        className="flex items-center justify-center py-2 px-3 rounded bg-primary-600 text-white w-2/3 text-sm cursor-pointer hover:bg-primary-500"
-                        href={`${book?.file}?dl=${slugify(book?.title)}.pdf`}
-                      >
-                        Download
-                      </a>
-                    </div>
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <div className="flex flex-col justify-center items-center space-y-3 p-2">
-                      <div>
-                        <Image
-                          alt={slugify(book?.title)}
-                          src={
-                            image || '/images/livingpupil-homeschool-logo.png'
-                          }
-                          width={150}
-                          height={250}
-                        />
-                      </div>
-                      <a
-                        className="flex items-center justify-center py-2 px-3 rounded bg-primary-600 text-white w-2/3 text-sm cursor-pointer hover:bg-primary-500"
-                        href={`${book?.file}?dl=${slugify(book?.title)}.pdf`}
-                      >
-                        Download
-                      </a>
-                    </div>
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <div className="flex flex-col justify-center items-center space-y-3 p-2">
-                      <div>
-                        <Image
-                          alt={slugify(book?.title)}
-                          src={
-                            image || '/images/livingpupil-homeschool-logo.png'
-                          }
-                          width={150}
-                          height={250}
-                        />
-                      </div>
-                      <a
-                        className="flex items-center justify-center py-2 px-3 rounded bg-primary-600 text-white w-2/3 text-sm cursor-pointer hover:bg-primary-500"
-                        href={`${book?.file}?dl=${slugify(book?.title)}.pdf`}
-                      >
-                        Download
-                      </a>
-                    </div>
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <div className="flex flex-col justify-center items-center space-y-3 p-2">
-                      <div>
-                        <Image
-                          alt={slugify(book?.title)}
-                          src={
-                            image || '/images/livingpupil-homeschool-logo.png'
-                          }
-                          width={150}
-                          height={250}
-                        />
-                      </div>
-                      <a
-                        className="flex items-center justify-center py-2 px-3 rounded bg-primary-600 text-white w-2/3 text-sm cursor-pointer hover:bg-primary-500"
-                        href={`${book?.file}?dl=${slugify(book?.title)}.pdf`}
-                      >
-                        Download
-                      </a>
-                    </div>
-                  </Carousel.Item>
-                  <Carousel.Item>
-                    <div className="flex flex-col justify-center items-center space-y-3 p-2">
-                      <div>
-                        <Image
-                          alt={slugify(book?.title)}
-                          src={
-                            image || '/images/livingpupil-homeschool-logo.png'
-                          }
-                          width={150}
-                          height={250}
-                        />
-                      </div>
-                      <a
-                        className="flex items-center justify-center py-2 px-3 rounded bg-primary-600 text-white w-2/3 text-sm cursor-pointer hover:bg-primary-500"
-                        href={`${book?.file}?dl=${slugify(book?.title)}.pdf`}
-                      >
-                        Download
-                      </a>
-                    </div>
-                  </Carousel.Item>
-                </Carousel>
+          {books?.length > 0 ? (
+            <>
+              <div className="flex flex-col space-y-5">
+                <div className="text-xl italic">Parents</div>
+                {parentBooks?.length > 0 ? (
+                  <div>
+                    <Carousel cols={5} rows={1} gap={10} loop>
+                      {parentBooks?.map((book, idx) => {
+                        const imageAsset = imageBuilder.image(
+                          book?.cover?.asset
+                        );
+                        const image = imageAsset?.options?.source
+                          ? imageAsset?.url()
+                          : null;
+                        return (
+                          <Carousel.Item key={idx}>
+                            <div className="flex flex-col justify-center items-center space-y-3 p-2">
+                              <div>
+                                <Image
+                                  alt={slugify(book?.title?.toLowerCase())}
+                                  src={
+                                    image ||
+                                    '/images/livingpupil-homeschool-logo.png'
+                                  }
+                                  width={150}
+                                  height={250}
+                                />
+                              </div>
+                              <a
+                                className="flex items-center justify-center py-2 px-3 rounded bg-primary-600 text-white w-2/3 text-sm cursor-pointer hover:bg-primary-500"
+                                href={`${book?.file}?dl=${slugify(
+                                  book?.title?.toLowerCase()
+                                )}.pdf`}
+                              >
+                                Download
+                              </a>
+                            </div>
+                          </Carousel.Item>
+                        );
+                      })}
+                    </Carousel>
+                  </div>
+                ) : (
+                  <div className="text=3xl italic">
+                    No available books for parents...
+                  </div>
+                )}
               </div>
-            </div>
+              <div className="flex flex-col space-y-5">
+                <div className="text-xl italic">Students</div>
+                {studentBooks?.length > 0 ? (
+                  <div>
+                    <Carousel cols={5} rows={1} gap={10} loop>
+                      {studentBooks?.map((book, idx) => {
+                        const imageAsset = imageBuilder.image(
+                          book?.cover?.asset
+                        );
+                        const image = imageAsset?.options?.source
+                          ? imageAsset?.url()
+                          : null;
+                        return (
+                          <Carousel.Item key={idx}>
+                            <div className="flex flex-col justify-center items-center space-y-3 p-2">
+                              <div>
+                                <Image
+                                  alt={slugify(book?.title?.toLowerCase())}
+                                  src={
+                                    image ||
+                                    '/images/livingpupil-homeschool-logo.png'
+                                  }
+                                  width={150}
+                                  height={250}
+                                />
+                              </div>
+                              <a
+                                className="flex items-center justify-center py-2 px-3 rounded bg-primary-600 text-white w-2/3 text-sm cursor-pointer hover:bg-primary-500"
+                                href={`${book?.file}?dl=${slugify(
+                                  book?.title?.toLowerCase()
+                                )}.pdf`}
+                              >
+                                Download
+                              </a>
+                            </div>
+                          </Carousel.Item>
+                        );
+                      })}
+                    </Carousel>
+                  </div>
+                ) : (
+                  <div className="text=3xl italic">
+                    No available books for students...
+                  </div>
+                )}
+              </div>
+            </>
+          ) : (
+            <div className="text=3xl italic">No available books...</div>
           )}
-          <div>Students</div>
         </div>
       </Content.Container>
     </AccountLayout>
