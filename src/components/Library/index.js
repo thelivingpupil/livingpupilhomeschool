df6a3aa95dc9034b697a8ad4414ec8d6f038dfc4
Gideon Arces <gid.arces07@gmail.com>
2023-06-04 18:08:27 +0800

    Fixing import slugify on library item

--- src/components/Library/index.js
+++ src/components/Library/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
+import slugify from 'slugify';
 
 export default ({ book, image }) => {
   const [showDescription, setDescriptionVisibility] = useState(false);
