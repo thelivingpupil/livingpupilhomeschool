385aa1cea4201436bfc33f02977c83705193bf3c
Gideon Arces <gid.arces07@gmail.com>
2023-06-04 18:12:28 +0800

    Fixing issue on alignment for description on library

--- src/components/Library/index.js
+++ src/components/Library/index.js
@@ -18,19 +18,21 @@ export default ({ book, image }) => {
         />
       </div>
       <div className="font-bold">{book?.title}</div>
-      <button
-        className="px-3 py-1 text-sm underline bg-gray-100 rounded text-secondary-800 hover:text-secondary-600"
-        onClick={toggle}
-      >
-        {showDescription ? 'Hide' : 'Read'} Item Description &raquo;
-      </button>
-      {showDescription
-        ? book?.description && (
-            <div className="flex items-start justify-center py-2 space-y-3 text-sm">
-              {book?.description}
-            </div>
-          )
-        : null}
+      <div className="flex items-start">
+        <button
+          className="px-3 py-1 text-sm underline bg-gray-100 rounded text-secondary-800 hover:text-secondary-600"
+          onClick={toggle}
+        >
+          {showDescription ? 'Hide' : 'Read'} Item Description &raquo;
+        </button>
+        {showDescription
+          ? book?.description && (
+              <div className="flex items-start justify-center py-2 space-y-3 text-sm">
+                {book?.description}
+              </div>
+            )
+          : null}
+      </div>
       <a
         className="flex items-center justify-center py-2 px-3 rounded bg-primary-600 text-white w-2/3 text-sm cursor-pointer hover:bg-primary-500"
         href={`${book?.file}?dl=${slugify(book?.title?.toLowerCase())}.pdf`}
