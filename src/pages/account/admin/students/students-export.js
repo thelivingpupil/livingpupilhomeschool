fdda04fde48551a41c18cdc17c3c50bc693dbeed
Gideon Arces <gid.arces07@gmail.com>
2023-06-10 13:48:34 +0800

    Trying sample data for exporting student list

--- src/pages/account/admin/students/students-export.js
+++ src/pages/account/admin/students/students-export.js
@@ -10,82 +10,94 @@ import Card from '@/components/Card';
 
 const StudentsExport = ({ data }) => {
   console.log('data export', data);
+
   const headers = [
-    { key: 'studentId', label: 'Student ID ' },
-    { key: 'firstName', label: 'First Name' },
-    { key: 'middleName', label: 'Middle Name' },
-    { key: 'lastName', label: 'Last Name' },
-    { key: 'birthDate', label: 'Birth Date' },
-    { key: 'gender', label: 'Gender' },
-    { key: 'religion', label: 'Religion' },
-    { key: 'incomingGradeLevel', label: 'Grade Level' },
-    { key: 'enrollmentType', label: 'Enrollment Type' },
-    { key: 'program', label: 'Program' },
-    { key: 'accreditation', label: 'Accreditation' },
-    { key: 'reason', label: 'Homeschooling Reason' },
-    { key: 'formerSchoolName', label: 'Former School Name' },
-    { key: 'formerSchoolAddress', label: 'Former School Address' },
-    { key: 'image', label: 'Student Picture' },
-    { key: 'liveBirthCertificate', label: 'Live Birth Certificate' },
-    { key: 'reportCard', label: 'Report Card' },
-    { key: 'createdAt', label: 'Enrollment Date' },
-    { key: 'student.name', label: 'Student Record Alias ' },
-    { key: 'student.slug', label: 'Student Record Unique Key' },
-    // { key: 'student.creator.email', label: 'Account Email' },
-    // { key: 'student.creator.emailVerified', label: 'Date Verified' },
-    // {
-    //   key: 'student.creator.guardianInformation.primaryGuardianName',
-    //   label: 'Primary Guardian Name',
-    // },
-    // {
-    //   key: 'student.creator.guardianInformation.primaryGuardianOccupation',
-    //   label: 'Primary Guardian Occupation',
-    // },
-    // {
-    //   key: 'student.creator.guardianInformation.primaryGuardianProfile',
-    //   label: 'Primary Guardian Facebook Profile',
-    // },
-    // {
-    //   key: 'student.creator.guardianInformation.primaryGuardianType',
-    //   label: 'Primary Guardian Type',
-    // },
-    // {
-    //   key: 'student.creator.guardianInformation.secondaryGuardianName',
-    //   label: 'Secondary Guardian Name',
-    // },
-    // {
-    //   key: 'student.creator.guardianInformation.secondaryGuardianOccupation',
-    //   label: 'Secondary Guardian Occupation',
-    // },
-    // {
-    //   key: 'student.creator.guardianInformation.secondaryGuardianProfile',
-    //   label: 'Secondary Guardian Facebook Profile',
-    // },
-    // {
-    //   key: 'student.creator.guardianInformation.secondaryGuardianType',
-    //   label: 'Secondary Guardian Type',
-    // },
-    // {
-    //   key: 'student.creator.guardianInformation.address1',
-    //   label: 'Address Line 1',
-    // },
-    // {
-    //   key: 'student.creator.guardianInformation.address2',
-    //   label: 'Address Line 2',
-    // },
-    // {
-    //   key: 'student.creator.guardianInformation.anotherEmail',
-    //   label: 'Secondary Email',
-    // },
-    // {
-    //   key: 'student.creator.guardianInformation.mobileNumber',
-    //   label: 'Mobile Number',
-    // },
-    // {
-    //   key: 'student.creator.guardianInformation.telephoneNumber',
-    //   label: 'Telephone Number',
-    // },
+    { label: 'First Name', key: 'firstname' },
+    { label: 'Last Name', key: 'lastname' },
+    { label: 'Email', key: 'email' },
   ];
+
+  const sampleData = [
+    { firstname: 'Ahmed', lastname: 'Tomi', email: 'ah@smthing.co.com' },
+    { firstname: 'Raed', lastname: 'Labes', email: 'rl@smthing.co.com' },
+    { firstname: 'Yezzi', lastname: 'Min l3b', email: 'ymin@cocococo.com' },
+  ];
+  // const headers = [
+  //   { key: 'studentId', label: 'Student ID ' },
+  //   { key: 'firstName', label: 'First Name' },
+  //   { key: 'middleName', label: 'Middle Name' },
+  //   { key: 'lastName', label: 'Last Name' },
+  //   { key: 'birthDate', label: 'Birth Date' },
+  //   { key: 'gender', label: 'Gender' },
+  //   { key: 'religion', label: 'Religion' },
+  //   { key: 'incomingGradeLevel', label: 'Grade Level' },
+  //   { key: 'enrollmentType', label: 'Enrollment Type' },
+  //   { key: 'program', label: 'Program' },
+  //   { key: 'accreditation', label: 'Accreditation' },
+  //   { key: 'reason', label: 'Homeschooling Reason' },
+  //   { key: 'formerSchoolName', label: 'Former School Name' },
+  //   { key: 'formerSchoolAddress', label: 'Former School Address' },
+  // { key: 'image', label: 'Student Picture' },
+  // { key: 'liveBirthCertificate', label: 'Live Birth Certificate' },
+  // { key: 'reportCard', label: 'Report Card' },
+  // { key: 'createdAt', label: 'Enrollment Date' },
+  // { key: 'student.name', label: 'Student Record Alias ' },
+  // { key: 'student.slug', label: 'Student Record Unique Key' },
+  // { key: 'student.creator.email', label: 'Account Email' },
+  // { key: 'student.creator.emailVerified', label: 'Date Verified' },
+  // {
+  //   key: 'student.creator.guardianInformation.primaryGuardianName',
+  //   label: 'Primary Guardian Name',
+  // },
+  // {
+  //   key: 'student.creator.guardianInformation.primaryGuardianOccupation',
+  //   label: 'Primary Guardian Occupation',
+  // },
+  // {
+  //   key: 'student.creator.guardianInformation.primaryGuardianProfile',
+  //   label: 'Primary Guardian Facebook Profile',
+  // },
+  // {
+  //   key: 'student.creator.guardianInformation.primaryGuardianType',
+  //   label: 'Primary Guardian Type',
+  // },
+  // {
+  //   key: 'student.creator.guardianInformation.secondaryGuardianName',
+  //   label: 'Secondary Guardian Name',
+  // },
+  // {
+  //   key: 'student.creator.guardianInformation.secondaryGuardianOccupation',
+  //   label: 'Secondary Guardian Occupation',
+  // },
+  // {
+  //   key: 'student.creator.guardianInformation.secondaryGuardianProfile',
+  //   label: 'Secondary Guardian Facebook Profile',
+  // },
+  // {
+  //   key: 'student.creator.guardianInformation.secondaryGuardianType',
+  //   label: 'Secondary Guardian Type',
+  // },
+  // {
+  //   key: 'student.creator.guardianInformation.address1',
+  //   label: 'Address Line 1',
+  // },
+  // {
+  //   key: 'student.creator.guardianInformation.address2',
+  //   label: 'Address Line 2',
+  // },
+  // {
+  //   key: 'student.creator.guardianInformation.anotherEmail',
+  //   label: 'Secondary Email',
+  // },
+  // {
+  //   key: 'student.creator.guardianInformation.mobileNumber',
+  //   label: 'Mobile Number',
+  // },
+  // {
+  //   key: 'student.creator.guardianInformation.telephoneNumber',
+  //   label: 'Telephone Number',
+  // },
+  // ];
 
   return (
     <AdminLayout>
@@ -97,7 +109,7 @@ const StudentsExport = ({ data }) => {
       <Content.Divider />
       <CSVLink
         className="items-center px-3 py-3 space-x-2 text-sm text-center text-white rounded bg-primary-500 hover:bg-secondary-500 hover:text-primary-600"
-        data={data}
+        data={sampleData}
         filename={`students-export-${format(
           new Date(),
           'yyyy.MM.dd.kk.mm.ss'
