9e9a8605c3d22c3515088f6d3e53846b8eb8c278
Gideon Arces <gid.arces07@gmail.com>
2023-08-07 13:21:25 +0800

    Fixing issue on menuItem

--- src/config/menu/index.js
+++ src/config/menu/index.js
@@ -10,7 +10,7 @@ const menu = (workspaceId) => [
       {
         name: 'Grades',
         path: `/account/${workspaceId}/grades`,
-        showDefault: true,
+        showDefault: false,
         validateItem: (validate) => {
           return validate;
         },
@@ -18,7 +18,7 @@ const menu = (workspaceId) => [
       {
         name: 'Courses and Training',
         path: `/account/${workspaceId}/training`,
-        showDefault: true,
+        showDefault: false,
         validateItem: (validate) => {
           return validate;
         },
