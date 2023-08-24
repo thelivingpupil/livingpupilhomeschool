bd0fdccaab50a3d2fd43a9d0b48bda53115efb6a
Gideon Arces <gid.arces07@gmail.com>
2023-08-24 17:34:48 +0800

    Fixing issue on CSS

--- src/pages/account/resources.js
+++ src/pages/account/resources.js
@@ -159,7 +159,7 @@ const Resources = ({ lessonPlans, blueprints }) => {
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 py-10">
               <div className="flex justify-center">
                 <a
-                  className={`flex items-center justify-center py-2 px-3 rounded bg-secondary-600 text-white w-full md:w-4/5 text-sm cursor-pointer hover:bg-secondary-500`}
+                  className={`flex items-center justify-center py-2 px-3 rounded bg-primary-600 text-white w-full md:w-4/5 text-sm cursor-pointer hover:bg-primary-500`}
                   href="/files/lp-faqs.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
@@ -169,7 +169,7 @@ const Resources = ({ lessonPlans, blueprints }) => {
               </div>
               <div className="flex justify-center">
                 <a
-                  className={`flex items-center justify-center py-2 px-3 rounded bg-primary-600 text-white w-full md:w-4/5 text-sm cursor-pointer hover:bg-primary-500`}
+                  className={`flex items-center justify-center py-2 px-3 rounded bg-secondary-600 text-white w-full md:w-4/5 text-sm cursor-pointer hover:bg-secondary-500`}
                   href="/files/lp-lingo.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
