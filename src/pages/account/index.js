a48dc0adace87222e6743cba2aa557480b237caf
julo07 <marjulmugot@gmail.com>
2024-03-20 12:55:36 +0800

    revert changes

--- src/pages/account/index.js
+++ src/pages/account/index.js
@@ -202,8 +202,7 @@ const Welcome = () => {
                     </button>
                   </Card.Footer>
                 )}
-                {/* workspace.studentRecord.schoolYear === '2023' || */}
-                {workspace.studentRecord != null &&  workspace.studentRecord.schoolYear === '2022' && (
+                {workspace.studentRecord != null && workspace.studentRecord.schoolYear === '2023' && (
                   <Card.Footer>
                     <button
                       className="text-primary-600"
