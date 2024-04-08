24960306c1e60e88a8d8fd1ac6c350113027bb17
julo07 <marjulmugot@gmail.com>
2024-04-08 20:25:16 +0800

    embed jotforms

--- src/pages/account/[workspaceSlug]/grades.js
+++ src/pages/account/[workspaceSlug]/grades.js
@@ -7,17 +7,17 @@ import JotFormEmbed from 'react-jotform-embed';
 import { GradeLevel } from '@prisma/client';
 
 const forms = {
-  [GradeLevel.K2]: '231091044111438',
-  [GradeLevel.GRADE_1]: '230861217347455',
-  [GradeLevel.GRADE_2]: '230861217347455',
-  [GradeLevel.GRADE_3]: '230861217347455',
-  [GradeLevel.GRADE_4]: '231090829906460',
-  [GradeLevel.GRADE_5]: '231090829906460',
-  [GradeLevel.GRADE_6]: '231090829906460',
-  [GradeLevel.GRADE_7]: '231090829906460',
-  [GradeLevel.GRADE_8]: '231090829906460',
-  [GradeLevel.GRADE_9]: '231090829906460',
-  [GradeLevel.GRADE_10]: '231090829906460',
+  [GradeLevel.K2]: '240981686870470',
+  [GradeLevel.GRADE_1]: '240981690530458',
+  [GradeLevel.GRADE_2]: '240981690530458',
+  [GradeLevel.GRADE_3]: '240981690530458',
+  [GradeLevel.GRADE_4]: '240980686578474',
+  [GradeLevel.GRADE_5]: '240980686578474',
+  [GradeLevel.GRADE_6]: '240980686578474',
+  [GradeLevel.GRADE_7]: '240980946894474',
+  [GradeLevel.GRADE_8]: '240980946894474',
+  [GradeLevel.GRADE_9]: '240980946894474',
+  [GradeLevel.GRADE_10]: '240980946894474',
 };
 
 const Grades = () => {
