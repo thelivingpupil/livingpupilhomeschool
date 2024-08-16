cfb7e483aab9f603de4788e3c89ec87e046b2fc6
julo07 <114547558+julo07@users.noreply.github.com>
2024-08-17 02:15:06 +0800

    add event file

--- src/pages/account/calendar.js
+++ src/pages/account/calendar.js
@@ -21,6 +21,7 @@ const types = {
 const Calendar = ({ events }) => {
   const [expandedImage, setExpandedImage] = useState(null);
   const [expandedEventId, setExpandedEventId] = useState(null);
+  console.log(events)
 
   const handleImageClick = (imageUrl) => {
     setExpandedImage(imageUrl);
@@ -65,8 +66,8 @@ const Calendar = ({ events }) => {
             return (
               <div className="cols-span-auto">
                 <Card key={event._id}>
-                  <div 
-                    className="relative inline-block w-full cursor-pointer" 
+                  <div
+                    className="relative inline-block w-full cursor-pointer"
                     onClick={() => handleImageClick(image)}
                     onMouseEnter={(e) => handleImageHover(e, event._id)}
                     onMouseLeave={handleImageLeave}
@@ -91,7 +92,7 @@ const Calendar = ({ events }) => {
                       </div>
                     )}
                   </div>
-                  
+
                   <div className="flex text-xl font-bold mt-4">
                     {event.title}
                   </div>
@@ -100,86 +101,107 @@ const Calendar = ({ events }) => {
                   </div>
                   {/* Button to toggle additional info */}
                   <button
-                    className="text-sm text-gray-600 bg-gray-100 rounded-full px-3 py-1 mt-2"
+                    className="text-sm text-gray-600 bg-gray-100 rounded-full px-3 py-1 mt-1 mb-4"
                     onClick={() => handleToggleAdditionalInfo(event._id)}
                   >
                     {expandedEventId === event._id ? 'Click To Hide Event Details' : 'Click To Show Event Details'}
                   </button>
                   {expandedEventId === event._id && (
                     <>
-                    
-                  <div className="flex flex-wrap">
-                    {event.types
-                      ?.filter((type) => type !== '')
-                      ?.map((type, index) => (
-                        <span
-                          key={index}
-                          className="px-3 py-1 mb-2 mr-3 text-xs text-gray-600 bg-gray-100 rounded-full"
-                        >
-                          {types[type]}
-                        </span>
-                      ))}
-                  </div>
-                  <div className="flex flex-col mt-4">
-                    <div className="flex font-semibold">Date and Times:</div>
-                    <div className="flex flex-wrap">
-                      {event.dateandtime.map((date, idx) => (
-                        <div
-                          className="flex text-sm text-gray-600 bg-gray-100 rounded-full px-3 py-1 mb-2 mr-3"
-                          key={idx}
-                        >
-                          {format(
-                            new Date(date),
-                            'eeee, MMMM dd, yyyy hh:mm aa'
+
+                      <div className="flex flex-wrap">
+                        {event.types
+                          ?.filter((type) => type !== '')
+                          ?.map((type, index) => (
+                            <span
+                              key={index}
+                              className="px-3 py-1 mb-2 mr-3 text-xs text-gray-600 bg-gray-100 rounded-full"
+                            >
+                              {types[type]}
+                            </span>
+                          ))}
+                      </div>
+                      <div className="flex flex-col mt-4">
+                        <div className="flex font-semibold">Date and Times:</div>
+                        <div className="flex flex-wrap">
+                          {event.dateandtime.map((date, idx) => (
+                            <div
+                              className="flex text-sm text-gray-600 bg-gray-100 rounded-full px-3 py-1 mb-2 mr-3"
+                              key={idx}
+                            >
+                              {format(
+                                new Date(date),
+                                'eeee, MMMM dd, yyyy hh:mm aa'
+                              )}
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                      <div className="flex flex-col mt-4">
+                        <div className="flex font-semibold">Who can join?</div>
+                        <div className="flex flex-col">
+                          {event.joiners
+                            ?.filter((joiner) => joiner !== '')
+                            ?.map((joiner, idx) => (
+                              <div
+                                className="flex text-sm text-gray-600 bg-gray-100 px-3 py-1 mb-2 mr-3"
+                                key={idx}
+                              >
+                                {joiner}
+                              </div>
+                            ))}
+                        </div>
+                      </div>
+                      <div className="flex flex-col mt-4">
+                        <div className="flex font-semibold">Registration Link</div>
+                        <div className="flex flex-col mt-2">
+                          <a
+                            href={event.link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-sm break-words"
+                          >
+                            {event.link}
+                          </a>
+                        </div>
+                      </div>
+                      <div className="flex flex-col mt-4">
+                        <div className="flex font-semibold">Event File</div>
+                        <div className="flex flex-col mt-2">
+                          {event.eventFile?.url ? (
+                            <a
+                              href={event.eventFile.url}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                              className="text-sm break-words bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600 text-center inline-block"
+                            >
+                              Download File
+                            </a>
+                          ) : (
+                            <span className="text-sm break-words bg-gray-100 text-gray-600 py-2 px-4 rounded-full">
+                              No file available
+                            </span>
                           )}
                         </div>
-                      ))}
-                    </div>
-                  </div>
-                  <div className="flex flex-col mt-4">
-                    <div className="flex font-semibold">Who can join?</div>
-                    <div className="flex flex-col">
-                      {event.joiners
-                        ?.filter((joiner) => joiner !== '')
-                        ?.map((joiner, idx) => (
-                          <div
-                            className="flex text-sm text-gray-600 bg-gray-100 px-3 py-1 mb-2 mr-3"
-                            key={idx}
+                      </div>
+
+
+                      <div className="flex flex-col mt-4">
+                        <div className="flex font-semibold">Venue</div>
+                        <div className="flex flex-col mt-2">
+                          <a
+                            href={event.maplink}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-sm break-words"
                           >
-                            {joiner}
-                          </div>
-                        ))}
-                    </div>
-                  </div>
-                  <div className="flex flex-col mt-4">
-                    <div className="flex font-semibold">Registration Link</div>
-                    <div className="flex flex-col mt-2">
-                      <a
-                        href={event.link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-sm break-words"
-                      >
-                        {event.link}
-                      </a>
-                    </div>
-                  </div>
-                  <div className="flex flex-col mt-4">
-                    <div className="flex font-semibold">Venue</div>
-                    <div className="flex flex-col mt-2">
-                      <a
-                        href={event.maplink}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-sm break-words"
-                      >
-                        {event.maplink}
-                      </a>
-                    </div>
-                  </div>
+                            {event.maplink}
+                          </a>
+                        </div>
+                      </div>
                     </>
                   )}
-                  
+
                 </Card>
               </div>
             );
@@ -188,14 +210,29 @@ const Calendar = ({ events }) => {
       </Content.Container>
       {/* Expanded image modal */}
       {expandedImage && (
-        <ImageModal imageUrl={expandedImage} onClose={handleCloseExpandedImage} className/>
+        <ImageModal imageUrl={expandedImage} onClose={handleCloseExpandedImage} className />
       )}
     </AccountLayout>
   );
 };
 
 export const getServerSideProps = async () => {
-  const events = await sanityClient.fetch(`*[_type == 'events']`);
+  const events = await sanityClient.fetch(`*[_type == 'events']{
+    _id,
+    title,
+    dateandtime,
+    description,
+    joiners,
+    poster,
+    types,
+    eventFile {
+      'url': asset->url
+    },
+    link,
+    maplink,
+    _createdAt,
+    _updatedAt
+  }`);
 
   return {
     props: {
@@ -203,5 +240,6 @@ export const getServerSideProps = async () => {
     },
   };
 };
+
 
 export default Calendar;
