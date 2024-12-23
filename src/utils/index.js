8aba25b9428fed222d7c93ef345117e5ae06c20f
juloh07 <marjulmugot@gmail.com>
2024-12-23 10:20:29 +0800

    edit  deadline

--- src/utils/index.js
+++ src/utils/index.js
@@ -56,7 +56,7 @@ const deadlines = {
     // July
     {
       [PaymentType.ANNUAL]: [0],
-      [PaymentType.SEMI_ANNUAL]: [0, 3, 8],
+      [PaymentType.SEMI_ANNUAL]: [0, 3, 6],
       [PaymentType.QUARTERLY]: [0, 3, 6, 8],
       [PaymentType.MONTHLY]: [0, 1, 2, 3, 4, 5, 6, 7, 8],
       [PaymentType.PAY_ALL]: [0],
