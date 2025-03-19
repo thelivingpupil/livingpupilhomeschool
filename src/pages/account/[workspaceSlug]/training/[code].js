c4fc0665e236c6df29056cf7418ff9bd3178cd17
juloh07 <marjulmugot@gmail.com>
2025-03-19 17:45:53 +0800

    fix parentTraining id

--- src/pages/account/[workspaceSlug]/training/[code].js
+++ src/pages/account/[workspaceSlug]/training/[code].js
@@ -13,14 +13,12 @@ import { PARENT_TRAINING_CODES } from '@/utils/constants';
 
 const Course = ({ course }) => {
   const { workspace } = useWorkspace();
-  const parentCourses = workspace?.creator?.guardianInformation?.parentTraining
-    ? workspace.creator.guardianInformation.parentTraining.map(
-      (training) => training.courseCode
-    )
-    : [];
-  const courseCode = course.code;
-  const guardianId = workspace?.creator.guardianInformation.id
-  const schoolYear = workspace?.studentRecord.schoolYear
+  const parentCourses = workspace?.creator?.guardianInformation?.parentTraining?.map(
+    (training) => training?.courseCode ?? ''
+  ) ?? [];
+  const courseCode = course?.code ?? '';
+  const guardianId = workspace?.creator?.guardianInformation?.id ?? null;
+  const schoolYear = workspace?.studentRecord?.schoolYear ?? '';
   const [isSubmitting, setSubmittingState] = useState(false);
 
   const filteredTraining = workspace?.creator?.guardianInformation?.parentTraining
