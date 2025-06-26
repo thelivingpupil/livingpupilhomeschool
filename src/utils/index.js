8a746d8ea891908c3f12f4f6f24a0fe0e6f73b72
juloh07 <157773027+juloh07@users.noreply.github.com>
2025-06-26 15:54:19 +0800

    update april quarter schedules

--- src/utils/index.js
+++ src/utils/index.js
@@ -233,7 +233,7 @@ const deadlines2025 = {
     {
       [PaymentType.ANNUAL]: [0],
       [PaymentType.SEMI_ANNUAL]: [0, 3, 7],
-      [PaymentType.QUARTERLY]: [0, 3, 7, 9],
+      [PaymentType.QUARTERLY]: [0, 3, 6, 9],
       [PaymentType.MONTHLY]: [0, 1, 2, 3, 4, 5, 6, 7, 8],
       [PaymentType.PAY_ALL]: [0],
     },
