e832c618af110ab1093fe602341c28a5d8060651
juloh07 <157773027+juloh07@users.noreply.github.com>
2025-07-03 18:39:30 +0800

    fix imports

--- src/pages/api/admin/affiliates/debug.js
+++ src/pages/api/admin/affiliates/debug.js
@@ -1,5 +1,5 @@
-import { getAffiliateDataPerYear } from '../../../../prisma/services/user.js';
-import { prisma } from '../../../../prisma/index.js';
+import { getAffiliateDataPerYear } from '@/prisma/services/user.js';
+import prisma from '@/prisma/index';
 
 export default async function handler(req, res) {
     if (req.method !== 'GET') {
