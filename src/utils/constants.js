43f6723e9c8b5310a7f49d9942d3e5486ab5301e
juloh07 <157773027+juloh07@users.noreply.github.com>
2026-06-10 01:21:07 +0800

    Update parent training descriptions and codes for preschool in constants.js to enhance curriculum clarity and organization.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -647,7 +647,16 @@ const GRADES_1_TO_10_PARENT_TRAINING = {
 
 export const PARENT_TRAINING_PER_GRADE_LEVEL = {
   PRESCHOOL: {
-    PT102025: '1. Developing a Rhytmn: Education is Life',
+    PT12025:
+      'The Heart of Learning: Cultivating Atmosphere, Discipline, and Life',
+    PT22025: '2. The Feast (K, Form 1,2,3)',
+    PT42025:
+      'Wonder and Wisdom: The Importance of Nature Study in Charlotte Mason Education',
+    PT62025:
+      'The riches in Common Subjects: Recitation, Hymn, Folksongs, Composer Study, Artist Study',
+    PT82025: 'The Blueprint',
+    PT92025: 'The Timetable',
+    PT102025: 'Developing a Rhytmn: Education is Life',
   },
   K1: {
     PT12025:
